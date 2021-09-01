381a3997892f1b6c8fcc74ca64de6c722b1ea69a
Matthew <barbaramatthew90@gmail.com>
2021-09-01 10:11:14 +0200

    initiated project and created dashboard page

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple web app that display Github users
---
+++ src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import './Dashboard.css';
+
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+      Github users:
+      {/* TODO: list github users here */}
+    </div>
+  );
+};
+
+export default Dashboard;
